2255524b5c217292c6d3be6a26a3822e223aabf7
sangbinlee <sangbinlee9@gmail.com>
2024-08-06 03:34:19 +0900

    spring boot 3 rest api cors 허용 in vuejs 3

--- src/main/java/com/sodi9/common/config/SecurityConfig.java
+++ src/main/java/com/sodi9/common/config/SecurityConfig.java
@@ -1,11 +1,16 @@
 package com.sodi9.common.config;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -14,9 +19,14 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.csrf(AbstractHttpConfigurer::disable);
+		http.httpBasic(HttpBasicConfigurer::disable);
+		http.cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource()));
+		
+		
 		http.authorizeHttpRequests(authorize -> 
 			authorize
-				.requestMatchers("/member/join").permitAll()
+			.requestMatchers("/api/employees").permitAll()
+			.requestMatchers("/member/join").permitAll()
 				.requestMatchers("/auth/login").permitAll()
 				.requestMatchers("/").permitAll()
 				.requestMatchers("/v3/api-docs/**",
@@ -25,4 +35,27 @@ public class SecurityConfig {
 				.anyRequest().authenticated());
 		return http.build();
 	}
+	
+	
+	
+	
+	
+	
+	
+
+	// ⭐️ CORS 설정
+    CorsConfigurationSource corsConfigurationSource() {
+        return request -> {
+            CorsConfiguration config = new CorsConfiguration();
+            config.setAllowedHeaders(Collections.singletonList("*"));
+            config.setAllowedMethods(Collections.singletonList("*"));
+            config.setAllowedOriginPatterns(Collections.singletonList(
+//            		"http://localhost:5173" 
+//            		"http://localhost:3000"
+            		"*"
+            		)); // ⭐️ 허용할 origin
+            config.setAllowCredentials(true);
+            return config;
+        };
+    }
 }
